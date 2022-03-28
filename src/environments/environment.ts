43baaf8af77614e51540d3f1a59838e8b061da5a
jassmanni <jassmanniq@gmail.com>
2022-03-28 18:54:50 -0400

    fixed otrografic erros

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://lainech.herokuapp.com',
   token:
     'pk.eyJ1IjoiZ3J1cG8yMDIxIiwiYSI6ImNreWNkY2ZqNzA2bmkzMm80cGgzb3oxemgifQ.M8px6pEr1d26y4UJUnaEYw',
 };
